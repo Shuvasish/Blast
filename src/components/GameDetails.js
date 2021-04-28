7489cfa875b03edde92ec2a515346fefabdad124
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-04-28 10:45:15 +0600

    added dynamic stars

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -18,6 +18,9 @@ import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
 
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
+
 const GameDetails = () => {
   const history = useHistory();
   //EXIT DETAIL
@@ -48,6 +51,20 @@ const GameDetails = () => {
     }
   };
 
+  //GET STAR IMAGESconst getStars = () => {
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img alt="star" key={i} src={starFull} />);
+      } else {
+        stars.push(<img alt="star" key={i} src={starEmpty} />);
+      }
+    }
+    return stars;
+  };
+
   //DATA
   const { game, screen, isLoading } = useSelector((state) => state.details);
 
@@ -59,7 +76,8 @@ const GameDetails = () => {
             <StyledStats>
               <div className="rating">
                 <h3>{game.name}</h3>
-                <p>Rating: {game.rating}</p>
+
+                {getStars()}
               </div>
               <StyledInfo>
                 <h3>Platforms</h3>
@@ -139,6 +157,11 @@ const StyledStats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const StyledInfo = styled(motion.div)`
